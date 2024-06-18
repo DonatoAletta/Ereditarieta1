2cd857787924e0048f61db2370309b48deec1fe4
DonatoAletta <donatoalettawork@gmail.com>
2024-06-18 09:50:47 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Animale animale = new Animale();
+        Animale gatto = new Gatto();
+
+        animale.faiIlVerso();
+        gatto.faiIlVerso();
+    }
+}
